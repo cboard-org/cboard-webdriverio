8c6454fdbc70e14d6084c2f226cca33c7ec428f0
martin bedouret <martinbedouret@gmail.com>
2018-11-05 11:40:25 -0300

    wip

---
+++ data/test-env.js
@@ -0,0 +1,7 @@
+exports.env = {
+    envTorun: 'qaEnv', // either qaEnv   or prodEnv
+
+    qaEnv: { url: 'https://app.qa.cboard.io', username: 'support@cboard.io', password: '1122' },
+    prodEnv: { url: 'https://app.cboard.io', username: 'support@cboard.io', password: '1122'  },
+
+};
