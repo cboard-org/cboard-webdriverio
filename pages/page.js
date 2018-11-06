5999bb3ce70814cf056e7ca0184459586b12f824
Martin Di Palma <tincho.dipalma@gmail.com>
2018-11-06 15:08:43 -0300

    Login spec scenarios

--- pages/page.js
+++ pages/page.js
@@ -1,8 +1,22 @@
+require('dotenv').config();
+
 function Page () {
 }
 
+
 Page.prototype.open = function (path) {
     browser.url(path);
+}
+
+Page.prototype.waitForPage = function (reload = false) {
+    if (reload) {
+        browser.reload();
+    }
+    this.open();
+
+    browser.waitUntil(() => {
+        return browser.element('.Cboard__DisplaySettings').type !== 'NoSuchElement';
+    }, 2000, 'App was not loaded');
 }
 
 module.exports = new Page()
--- test/specs/login.app.spec.js
+++ test/specs/login.app.spec.js
@@ -1,25 +1,100 @@
 var welcomeAppPage = require('../../pages/welcome.app.page');
 var assert = require('assert');
 
+const ELEMENTS = {
+    welcomeContainer: '.WelcomeScreen',
+    welcomeMessage: '.WelcomeScreen__heading span',
+    openLoginButton: '.WelcomeScreen__button--login',
+    loginEmailInput: '.Login__form input[name=email]',
+    loginPasswordInput: '.Login__form input[name=password]',
+    loginSubmitButton: '.Login__form button[type=submit]',
+    openSignupButton: '.WelcomeScreen__button--signup',
+    signupNameInput: '.SignUp__form input[name=name]',
+    signupEmailInput: '.SignUp__form input[name=email]',
+    signupPasswordInput: '.SignUp__form input[name=password]',
+    signupPasswordConfirmInput: '.SignUp__form input[name=passwordConfirm]',
+    signupSubmitInput: '.SignUp__form button[type=submit]',
+    signupSuccessMessage: '.SignUp__status--success',
+    skipButton: '.WelcomeScreen__button--skip',
+};
+
 describe('Welcome page', function() {
-     it('should display the valid welcome message', function () {
-        welcomeAppPage.open();
-        
+    it('should display the valid welcome message', function () {
+        welcomeAppPage.waitForPage();    
+        assert.equal(browser.getText(ELEMENTS.welcomeMessage), 'Welcome to Cboard');
     });
+
     it('should successfully login a valid user', function () {
-        welcomeAppPage.open();
-        
+        welcomeAppPage.waitForPage();
+
+        const openLoginButton = browser.element(ELEMENTS.openLoginButton);
+        openLoginButton.click();
+
+        const email = browser.element(ELEMENTS.loginEmailInput);
+        const password = browser.element(ELEMENTS.loginPasswordInput);
+        const loginButton = browser.element(ELEMENTS.loginSubmitButton);
+
+        email.setValue(process.env.LOGIN_APP_SPEC_USERNAME || 'missing LOGIN_APP_SPEC_USERNAME env var');
+        password.setValue(process.env.LOGIN_APP_SPEC_PASSWORD || 'missing LOGIN_APP_SPEC_PASSWORD env var');
+        loginButton.click();
+
+        browser.pause(5000);
+        browser.waitUntil(() => {
+            return browser.element(ELEMENTS.welcomeContainer).type === 'NoSuchElement';
+        }, 5000, 'Login error - timeout');    
     });
+
     it('should reject a login with an invalid user', function () {
-        welcomeAppPage.open();
-        
+        welcomeAppPage.waitForPage(true);
+
+        const openLoginButton = browser.element('.WelcomeScreen__button--login');
+        openLoginButton.click();
+
+        const email = browser.element(ELEMENTS.loginEmailInput);
+        const password = browser.element(ELEMENTS.loginPasswordInput);
+        const loginButton = browser.element(ELEMENTS.loginSubmitButton);
+
+        email.setValue('som.eBA+D.EM.AI.L@email.io');
+        password.setValue('someBADPASSWORD!$X');
+        loginButton.click();
+
+        browser.pause(5000);
+
+        browser.waitUntil(() => {
+            return browser.element(ELEMENTS.welcomeContainer).type !== 'NoSuchElement';
+        }, 5000, 'Login success :/');
     });
+    
     it('should successfully signup a new valid user', function () {
-        welcomeAppPage.open();
+        welcomeAppPage.waitForPage(true);
+        const openSignupButton = browser.element(ELEMENTS.openSignupButton);
+        openSignupButton.click();
+    
+        const name = browser.element(ELEMENTS.signupNameInput);
+        const email = browser.element(ELEMENTS.signupEmailInput);
+        const password = browser.element(ELEMENTS.signupPasswordInput);
+        const passwordConfirm = browser.element(ELEMENTS.signupPasswordConfirmInput);
+        const signupButton = browser.element(ELEMENTS.signupSubmitInput);
+    
+        name.setValue('TEST_USER');
+        email.setValue(`som.eBA+D.EM.AI.L_${Math.round(Math.random() * 99999999)}@email.io`);
+        password.setValue('123456');
+        passwordConfirm.setValue('123456');
+        signupButton.click();
         
-    });    
+        browser.pause(5000);
+
+        browser.waitUntil(() => {
+            return browser.element(ELEMENTS.signupSuccessMessage).type !== 'NoSuchElement';
+        }, 5000, 'Signup without success :/');        
+    });
+   
     it('should skip login when user press skip button', function () {
-        welcomeAppPage.open();
-        
+        welcomeAppPage.waitForPage(true);
+        const skipButton = browser.element(ELEMENTS.skipButton);
+        skipButton.click();
+        browser.waitUntil(() => {
+            return browser.element(ELEMENTS.welcomeContainer).type === 'NoSuchElement';
+        }, 1000, 'Skip button error');
     });
 });
