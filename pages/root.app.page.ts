5b463dbc183dae509dc808909ccee7ac0b595ba8
martin bedouret <martinbedouret@gmail.com>
2019-01-28 18:06:26 -0300

    Root board tests

--- pages/root.app.page.ts
+++ pages/root.app.page.ts
@@ -1,5 +1,6 @@
 import { assert } from 'chai';
 import { expect } from 'chai';
+var rn = require('random-number');
 import Page from 'pages/Page';
 import WelcomeAppPage from 'pages/welcome.app.page';
 
@@ -8,7 +9,7 @@ class RootAppPage extends Page {
    * define elements
    */
   get rootboard() {
-    return $('=home');
+    return $('h2=home');
   }
   get unlock() {
     return $('//button[@aria-label="Unlock"]');
@@ -23,20 +24,31 @@ class RootAppPage extends Page {
     return $('//div[@class="Symbol"]');
   }
 
+  get tileButton() {
+    return $$('//button[@class="Tile"]');
+  }
+
   get settings() {
     return $('//*[@aria-label="Settings"]');
   }
   get fullScreen() {
     return $('//*[@aria-label="Full screen"]');
   }
-  get printBoard() {
-    return $('//*[@aria-label="Print Board"]');
-  }
   get communicatorBar() {
     return $('.CommunicatorToolbar.Board__communicator-toolbar');
   }
   get boardEditBar() {
     return $('.EditToolbar.Board__edit-toolbar');
+  }
+  get tilesInCommunicator() {
+    return $$('div.SymbolOutput__value');
+  }
+
+  get clear() {
+    return $('//*[@aria-label="Clear"]');
+  }
+  get printBoard() {
+    return $('//*[@aria-label="Print Board"]');
   }
   /**
    * define or overwrite page methods
@@ -50,8 +62,7 @@ class RootAppPage extends Page {
     assert.equal(title, 'Cboard - AAC Communication Board');
   }
   isRootBoard() {
-    const roo = $('h2=home');
-    return roo.isDisplayed();
+    return this.rootboard.waitForDisplayed(5000);
   }
   unblockSettings() {
     expect(this.lock.isDisplayed()).to.be.false;
@@ -87,6 +98,32 @@ class RootAppPage extends Page {
   }
   isBoardEditBarDisplayed() {
     return this.boardEditBar.isDisplayed();
+  }
+  isTileDisplayedInCommunicatorBar(label) {
+    return $(
+      '//div[@class="SymbolOutput"]//div[@class="Symbol__label" and text()="' +
+        label +
+        '"]'
+    ).isDisplayed();
+  }
+  clickOnRandomTileButton() {
+    this.tile.waitForDisplayed(4000);
+    var length = this.tileButton.length;
+    var options = {
+      min: 0,
+      max: length - 1,
+      integer: true
+    };
+    var index = rn(options);
+    $$('//button[@class="Tile"]')[index].click();
+    return $$('//button[@class="Tile"]')[index].getText();
+  }
+  countCommunicatorBarTiles() {
+    return this.tilesInCommunicator.length;
+  }
+
+  clearCommunicatorBarTiles() {
+    return this.clear.click();
   }
 }
 
--- test/specs/root.app.spec.ts
+++ test/specs/root.app.spec.ts
@@ -10,7 +10,7 @@ describe('Root board page', function() {
     browser.reloadSession();
   });
   it('should display the valid page title', function() {
-    RootAppPage.open();
+    expect(RootAppPage.isRootBoard(), 'Root board not detected').to.be.true;
   });
   it('should unblock settings after 4 clicks on lock icon', function() {
     RootAppPage.unblockSettings();
@@ -24,6 +24,20 @@ describe('Root board page', function() {
     expect(RootAppPage.isCommunicatorBarDisplayed()).to.be.true;
     expect(RootAppPage.isBoardEditBarDisplayed()).to.be.true;
   });
-  it('should insert pictograms in communication bar when they are clicked', function() {});
-  it('should delete pictograms when user clicks on X button', function() {});
+  it('should insert pictograms in communication bar when they are clicked', function() {
+    var label = RootAppPage.clickOnRandomTileButton();
+    expect(
+      RootAppPage.isTileDisplayedInCommunicatorBar(label),
+      'Tile not added to communication bar'
+    ).to.be.true;
+  });
+  it('should delete pictograms when user clicks on X button', function() {
+    RootAppPage.clickOnRandomTileButton();
+    RootAppPage.clickOnRandomTileButton();
+    RootAppPage.clickOnRandomTileButton();
+    RootAppPage.clickOnRandomTileButton();
+    expect(RootAppPage.countCommunicatorBarTiles()).to.equal(4);
+    RootAppPage.clearCommunicatorBarTiles();
+    expect(RootAppPage.countCommunicatorBarTiles()).to.equal(0);
+  });
 });
