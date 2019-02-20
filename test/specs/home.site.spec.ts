1708112e9fc16af494471ab0e5a46832ba9567d5
martin bedouret <martinbedouret@gmail.com>
2019-02-20 19:31:56 -0300

    updates for moving

--- test/specs/home.site.spec.ts
+++ test/specs/home.site.spec.ts
@@ -1,5 +1,6 @@
-import HomePage from 'pages/home.site.page';
 import { assert } from 'chai';
+
+import HomePage from 'pages/home.site.page';
 
 describe('cboard.io page', function() {
   it('should have the right title', function() {
@@ -7,6 +8,7 @@ describe('cboard.io page', function() {
     var title = browser.getTitle();
     assert.equal(title, 'Home | Cboard - Communication Board');
   });
+
   it('should open the app', function() {
     HomePage.open();
     HomePage.clickOnStartCboard();
