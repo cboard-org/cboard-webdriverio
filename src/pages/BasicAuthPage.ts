399ecd22d0fbf9860447574a8f965de70d883dd9
martinbedouret <martinbedouret@gmail.com>
2019-04-02 16:21:45 -0300

    Remove boilerplate

--- src/pages/BasicAuthPage.ts
+++
@@ -1,9 +0,0 @@
-import BasePage from 'src/pages/BasePage';
-
-class BasicAuthPage extends BasePage {
-    get message() {
-        return $('#content > div > p').getText();
-    }
-}
-
-export default new BasicAuthPage();
