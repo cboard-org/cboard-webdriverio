ae6d02b648c0cb07a65cdb872440a0f1d9f15b8a
martinbedouret <martinbedouret@gmail.com>
2019-04-02 14:07:02 -0300

    WIP

--- wdio.conf.js
+++ wdio.conf.js
@@ -54,7 +54,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 1,
         //
-        browserName: 'chrome'
+        browserName: 'firefox'
     }],
     //
     // ===================
