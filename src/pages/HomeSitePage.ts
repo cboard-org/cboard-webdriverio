de6716f4fd1ab9f155e6850715f4a9448b5c66d8
martinbedouret <martinbedouret@gmail.com>
2019-04-02 10:13:43 -0300

    wip

---
+++ src/pages/HomeSitePage.ts
@@ -0,0 +1,58 @@
+import BasePage from 'src/pages/BasePage;
+
+class HomePage extends BasePage {
+  /**
+   * define elements
+   **/
+  get startCboard() {
+    return browser.$('=Start Cboard');
+  }
+  get blog() {
+    return browser.$('=BLOG');
+  }
+  get about() {
+    return browser.$('=ABOUT');
+  }
+  get contact() {
+    return browser.$('=CONTACT');
+  }
+  get help() {
+    return browser.$('=HELP');
+  }
+  get github() {
+    return browser.$('=GITHUB');
+  }
+
+  /**
+   * define or overwrite page methods
+   */
+  open() {
+    super.open('https://www.cboard.io/');
+  }
+
+  clickOnStartCboard() {
+    this.startCboard.click();
+  }
+
+  clickOnBlogMenuItem() {
+    this.blog.click();
+  }
+
+  clickOnAboutMenuItem() {
+    this.about.click();
+  }
+
+  clickOnContactMenuItem() {
+    this.contact.click();
+  }
+
+  clickOnHelpMenuItem() {
+    this.help.click();
+  }
+
+  clickOnGithubMenuItem() {
+    this.github.click();
+  }
+}
+
+export default new HomePage();
