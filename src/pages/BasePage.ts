954da8ad499194c8d9a66cd22059d4d6b3e54cf7
martinbedouret <martinbedouret@gmail.com>
2019-04-02 09:53:20 -0300

    wip

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -1,5 +1,35 @@
 export default class BasePage {
+    /**
+     * define elements
+    */
     get h1() {
         return $('h1').getText();
     }
+
+    /**
+     * define or overwrite page methods
+     */
+    open(path) {
+        browser.url(path);
+    }
+
+    reload() {
+        browser.reloadSession();
+    }
+
+    waitForPage(reload = false) {
+        if (reload) {
+            this.reload();
+        }
+        browser.waitUntil(
+            () => {
+                return (
+                    browser.$('.Cboard__DisplaySettings').getValue() !== 'NoSuchElement'
+                );
+            },
+            2000,
+            'App was not loaded'
+        );
+    }
 }
+
