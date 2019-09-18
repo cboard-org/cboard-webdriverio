92096ff9dc4522fcc2af2a0da22561b70c07cf5e
martin bedouret <martinbedouret@gmail.com>
2019-09-18 16:42:07 -0300

    update settings

--- wdio.conf.js
+++ wdio.conf.js
@@ -43,7 +43,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 1,
+    maxInstances: 4,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -52,14 +52,14 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 1,
+        maxInstances: 2,
         //
         browser: 'chrome'
     }, {
         // maxInstances can get overwritten per capability. So if you have an in house Selenium
         // grid with only 5 firefox instance available you can make sure that not more than
         // 5 instance gets started at a time.
-        maxInstances: 1,
+        maxInstances: 2,
         browserName: 'firefox',
         browser: 'firefox'
     }],
@@ -83,7 +83,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://' + process.env.QA_SERVER_AUTH + '@app.qa.cboard.io',
+    baseUrl: 'https://app.qa.cboard.io',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 90000,
