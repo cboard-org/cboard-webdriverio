84fc936a6ef7f252ad228546f4edba798584f59b
martin bedouret <martinbedouret@gmail.com>
2019-09-19 12:59:57 -0300

    update type

--- src/pages/CreatePictoAppPage.ts
+++ src/pages/CreatePictoAppPage.ts
@@ -9,71 +9,69 @@ class CreatePictoAppPage extends BasePage {
    * define elements
    */
   get pageH6() {
-    return $('h6=Create tile');
+	return $('h6=Create tile');
   }
   get folder() {
-    return $('//input[@value="folder"]');
+	return $('//input[@value="folder"]');
   }
   get button() {
-    return $('//input[@value="button"]');
+	return $('//input[@value="button"]');
   }
   get nameInput() {
-    return $('//*[@name="name"]');
+	return $('//*[@name="name"]');
   }
   get labelInput() {
-    return $('#label');
+	return $('#label');
   }
   get vocalizationInput() {
-    return $('#vocalization');
+	return $('#vocalization');
   }
   get saveButton() {
-    return $('span=Save');
-    }
-    get searchImages() {
-        return $('//button[@aria-label="Symbol search"]');
-    }
-    get searchInput() {
-        return $('//input[@label="Search symbol library"]');
-    }
-    get firstSymbol() {
-        return $('//ul[@role="listbox"]//li[@role="option"]');
-    }
+	return $('span=Save');
+	}
+	get searchImages() {
+		return $('//button[@aria-label="Symbol search"]');
+	}
+	get searchInput() {
+		return $('//input[@label="Search symbol library"]');
+	}
+	get firstSymbol() {
+		return $('//ul[@role="listbox"]//li[@role="option"]');
+	}
 
-    
+	
   /**
    * define or overwrite page methods
    */
   open() {
-    browser.url('/');
-    WelcomeAppPage.loginUser('anything@cboard.io', '1122');
-    RootAppPage.unblockSettings();
-    RootAppPage.clickOnCreateTiles();
-    expect(this.pageH6.isDisplayed()).to.be.true;
+	browser.url('/');
+	WelcomeAppPage.loginUser('anything@cboard.io', '1122');
+	RootAppPage.unblockSettings();
+	RootAppPage.clickOnCreateTiles();
+	expect(this.pageH6.isDisplayed()).to.be.true;
   }
 
   reload() {
-    super.reload();
+	super.reload();
   }
 
   createPicto(label, vocalization, type = 'button', search = '') {
 
-      if (search !== '') {
-          this.searchImages.click();
-          for (var i = 0; i < search.length; i++) {
+	  if (search !== '') {
+		  this.searchImages.click();
+          this.searchInput.setValue(search);
+		  browser.pause(2000);
+		  
+		  this.firstSymbol.click();
+	  }
 
-              this.searchInput.setValue(search.charAt(i));
-              browser.pause(200);
-          }
-          this.firstSymbol.click();
-      }
+	  this.labelInput.setValue(label);
+	  this.vocalizationInput.setValue(vocalization);
 
-      this.labelInput.setValue(label);
-      this.vocalizationInput.setValue(vocalization);
-
-    if (type === 'folder') {
-      this.folder.click();
-    }
-    this.saveButton.click();
+	if (type === 'folder') {
+	  this.folder.click();
+	}
+	this.saveButton.click();
   }
 }
 
