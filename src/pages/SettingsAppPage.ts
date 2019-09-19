c00435fcbf08340c77d645eff322a67667045ec2
martin bedouret <martinbedouret@gmail.com>
2019-09-19 13:21:49 -0300

    update settings

--- src/pages/SettingsAppPage.ts
+++ src/pages/SettingsAppPage.ts
@@ -5,83 +5,84 @@ import RootAppPage from 'src/pages/RootAppPage';
 import WelcomeAppPage from 'src/pages/WelcomeAppPage';
 
 class SettingsAppPage extends BasePage {
-  /**
-   * define elements
-   */
-  get pageH6() {
-    return $('h6=Settings');
-  }
-  get language() {
-    return $('//a[@href="/settings/language"]');
-  }
-  get speech() {
-    return $('//a[@href="/settings/speech"]');
-  }
-  get export() {
-    return $('//a[@href="/settings/export"]');
-  }
-  get import() {
-    return $('//a[@href="/settings/import"]');
-  }
-  get display() {
-    return $('//a[@href="/settings/display"]');
-  }
-  get scanning() {
-    return $('//a[@href="/settings/scanning"]');
-  }
-  get navigation() {
-    return $('//a[@href="/settings/navigation"]');
-  }
-  get exportButton() {
-    return $('#export-button');
-  }
-  get exportFormatMenu() {
-    return $('//ul[@role="menu"]');
-  }
+    /**
+     * define elements
+     */
+    get pageH6() {
+        return $('h6=Settings');
+    }
+    get language() {
+        return $('//a[@href="/settings/language"]');
+    }
+    get speech() {
+        return $('//a[@href="/settings/speech"]');
+    }
+    get export() {
+        return $('//a[@href="/settings/export"]');
+    }
+    get import() {
+        return $('//a[@href="/settings/import"]');
+    }
+    get display() {
+        return $('//a[@href="/settings/display"]');
+    }
+    get scanning() {
+        return $('//a[@href="/settings/scanning"]');
+    }
+    get navigation() {
+        return $('//a[@href="/settings/navigation"]');
+    }
+    get exportButton() {
+        return $('#export-button');
+    }
+    get exportFormatMenu() {
+        return $('//ul[@role="menu"]');
+    }
 
-  /**
-   * define or overwrite page methods
-   */
-  open() {
-    browser.url('/');
-    WelcomeAppPage.loginUser('anything@cboard.io', '1122');
-    RootAppPage.unblockSettings();
-    RootAppPage.clickOnSetttings();
-    expect(this.pageH6.isDisplayed()).to.be.true;
-  }
+    /**
+     * define or overwrite page methods
+     */
+    open() {
+        browser.url('/');
+        WelcomeAppPage.loginUser('anything@cboard.io', '1122');
+        RootAppPage.unblockSettings();
+        RootAppPage.clickOnSetttings();
+        expect(this.pageH6.isDisplayed()).to.be.true;
+    }
 
-  reload() {
-    super.reload();
-  }
+    reload() {
+        super.reload();
+    }
 
     clickOnLanguage() {
         return this.language.click();
-  }
+    }
 
-  clickOnSpeech() {
-    return this.speech.click();
-  }
-  clickOnExport() {
-    return this.export.click();
-  }
+    clickOnSpeech() {
+        return this.speech.click();
+    }
+    clickOnExport() {
+        return this.export.click();
+    }
 
-  clickOnImport() {
-    return this.import.click();
-  }
+    clickOnImport() {
+        return this.import.click();
+    }
     clickOnDisplay() {
         return this.display.click();
-  }
+    }
     clickOnNavigation() {
+        this.navigation.waitForDisplayed(6000);
         return this.navigation.click();
-  }
+    }
     clickOnScanning() {
         return this.scanning.click();
-  }
+    }
 
-  exportBoard(format = 'Cboard') {
-    this.exportButton.click();
-    this.exportFormatMenu.selectByVisibleText(format);
-  }
+    exportBoard(format = 'Cboard') {
+        this.exportButton.click();
+        this.exportFormatMenu.selectByVisibleText(format);
+    }
 }
 
 export default new SettingsAppPage();
--- test/settingsApp.spec.ts
+++ test/settingsApp.spec.ts
@@ -2,105 +2,105 @@ import SettingsPage from 'src/pages/SettingsAppPage';
 import RootAppPage from 'src/pages/RootAppPage';
 import SettingsAppPage from 'src/pages/SettingsAppPage';
 
-describe('Language option', function() {
+describe('Language option', function () {
     beforeEach(function () {
         browser.reloadSession();
-    SettingsAppPage.open();
-  });
+        SettingsAppPage.open();
+    });
 
-  afterEach(function() {
-  });
+    afterEach(function () {
+    });
 
-  it('should display available languages list', function() {
-    SettingsPage.clickOnLanguage();
-    //SettingsPage.exportBoard();
-  });
+    it('should display available languages list', function () {
+        SettingsPage.clickOnLanguage();
+        //SettingsPage.exportBoard();
+    });
 });
 
-describe('Speech option', function() {
+describe('Speech option', function () {
     beforeEach(function () {
         browser.reloadSession();
-    SettingsAppPage.open();
-  });
+        SettingsAppPage.open();
+    });
 
-  afterEach(function() {
-  });
+    afterEach(function () {
+    });
 
-  it('should display available speech settings', function() {
-    SettingsPage.clickOnSpeech();
-    //SettingsPage.exportBoard();
-  });
+    it('should display available speech settings', function () {
+        SettingsPage.clickOnSpeech();
+        //SettingsPage.exportBoard();
+    });
 });
 
-describe('Import board option', function() {
+describe('Import board option', function () {
     beforeEach(function () {
         browser.reloadSession();
-    SettingsAppPage.open();
-  });
+        SettingsAppPage.open();
+    });
 
-  afterEach(function() {
-  });
-  it('should import and immediately display the board', function() {
-    SettingsAppPage.clickOnImport();
-  });
+    afterEach(function () {
+    });
+    it('should import and immediately display the board', function () {
+        SettingsAppPage.clickOnImport();
+    });
 });
 
-describe('Export board option', function() {
+describe('Export board option', function () {
     beforeEach(function () {
         browser.reloadSession();
-    SettingsAppPage.open();
-  });
+        SettingsAppPage.open();
+    });
 
-  afterEach(function() {
-  });
+    afterEach(function () {
+    });
 
-  it('should download the root board as a cboard json file', function() {
-    SettingsPage.clickOnExport();
-    //SettingsPage.exportBoard();
-  });
+    it('should download the root board as a cboard json file', function () {
+        SettingsPage.clickOnExport();
+        //SettingsPage.exportBoard();
+    });
 });
 
-describe('Display option', function() {
+describe('Display option', function () {
     beforeEach(function () {
         browser.reloadSession();
-    SettingsAppPage.open();
-  });
+        SettingsAppPage.open();
+    });
 
-  afterEach(function() {
-  });
+    afterEach(function () {
+    });
 
-  it('should list the display options ', function() {
-    SettingsPage.clickOnDisplay();
-    //SettingsPage.exportBoard();
-  });
+    it('should list the display options ', function () {
+        SettingsPage.clickOnDisplay();
+        //SettingsPage.exportBoard();
+    });
 });
 
-describe('Scanning option', function() {
+describe('Scanning option', function () {
     beforeEach(function () {
         browser.reloadSession();
-    SettingsAppPage.open();
-  });
+        SettingsAppPage.open();
+    });
 
-  afterEach(function() {
-  });
+    afterEach(function () {
+    });
 
-  it('should display the scanning options', function() {
-    SettingsPage.clickOnScanning();
-    //SettingsPage.exportBoard();
-  });
+    it('should display the scanning options', function () {
+        SettingsPage.clickOnScanning();
+        //SettingsPage.exportBoard();
+    });
 });
 
-describe('Navigation option', function() {
+describe('Navigation option', function () {
     beforeEach(function () {
         browser.reloadSession();
-    SettingsAppPage.open();
-  });
+        SettingsAppPage.open();
+    });
 
-  afterEach(function() {
-  });
+    afterEach(function () {
+    });
 
-  it('should display the Navigation options', function() {
-    SettingsPage.clickOnNavigation();
-    //SettingsPage.exportBoard();
-  });
+    it('should display the Navigation options', function () {
+        SettingsPage.clickOnNavigation();
+        //SettingsPage.exportBoard();
+    });
 });
