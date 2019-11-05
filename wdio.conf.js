e505cbf80c65c922d99d1ba40314cd7568ca6432
martin bedouret <martinbedouret@gmail.com>
2019-11-05 10:43:32 -0300

    remove safari

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,14 +52,9 @@ exports.config = {
       // maxInstances can get overwritten per capability. So if you have an in-house Selenium
       // grid with only 5 firefox instances available you can make sure that not more than
       // 5 instances get started at a time.
-      maxInstances: 2,
+      maxInstances: 3,
       browserName: 'chrome',
       browser: 'chrome'
-    }, {
-      maxInstances: 3,
-      browserName: 'safari',
-        browser: 'safari',
-      browserVersion: '12'
     }],
     //
     // ===================
