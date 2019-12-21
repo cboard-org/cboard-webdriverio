3d33eb0a420fd116bec61b8d57dbd7d02ab645ba
martin bedouret <martinbedouret@gmail.com>
2019-12-21 10:15:25 -0300

    added safari

--- wdio.conf.js
+++ wdio.conf.js
@@ -49,12 +49,17 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     capabilities: [{
-      // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-      // grid with only 5 firefox instances available you can make sure that not more than
-      // 5 instances get started at a time.
-      maxInstances: 3,
-      browserName: 'chrome',
-      browser: 'chrome'
+        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        // grid with only 5 firefox instances available you can make sure that not more than
+        // 5 instances get started at a time.
+        maxInstances: 3,
+        browserName: 'chrome',
+        browser: 'chrome'
+    }, {
+        maxInstances: 2,
+        browserName: 'safari',
+        browser: 'safari',
+        browserVersion: '12'
     }],
     //
     // ===================
