b6c0fa37344e62416b281cd85585057538c595c4
Martin Bedouret <martinbedouret@gmail.com>
2022-08-05 13:22:07 -0300

    wip

--- src/pages/PeopleAppPage.ts
+++ src/pages/PeopleAppPage.ts
@@ -36,7 +36,12 @@ class PeopleAppPage extends BasePage {
    */
   open() {
     browser.url('/');
-    WelcomeAppPage.loginUser();
+    WelcomeAppPage.loginSkip();
+    RootAppPage.skipTour();
+    RootAppPage.unblockSettings();
+    RootAppPage.skipTour();
+    RootAppPage.loginOrSignUp();
+    this.pageH6.waitForDisplayed(4000);
   }
 
 }
