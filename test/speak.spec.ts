bc94a18e91a4cf6e028179db89109c20fad8a1e8
Martin Bedouret <martinbedouret@gmail.com>
2023-03-05 01:06:16 -0300

    wip

--- test/speak.spec.ts
+++ test/speak.spec.ts
@@ -21,14 +21,14 @@ describe('App can speak and', function () {
         expect(RootAppPage.isSpeaking(), 'Speaking detected').to.be.false;
         RootAppPage.clickOnRandomTileButton();
         expect(RootAppPage.isSpeaking(), 'Speaking detected').to.be.true;
-        browser.waitUntil(() => RootAppPage.isSpeaking() === false, 10000);
+        //browser.waitUntil(() => RootAppPage.isSpeaking() === false, 10000);
     });
     it('should sound when output bar is clicked', function () {
         RootAppPage.clickOnRandomTileButton();
         browser.waitUntil(() => RootAppPage.isSpeaking() === false, 10000);
         RootAppPage.clickSymbolOutput();
         expect(RootAppPage.isSpeaking(), 'Speaking detected').to.be.true;
-        browser.waitUntil(() => RootAppPage.isSpeaking() === false, 10000);
+        //browser.waitUntil(() => RootAppPage.isSpeaking() === false, 10000);
     });
     it('should not sound when a tile folder is clicked', function () {
         expect(RootAppPage.isSpeaking(), 'Speaking detected').to.be.false;
